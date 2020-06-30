9e1be56e48d4354f95ed54bb3cc1d18a00932f72
Andrei Lapets <a@lapets.io>
2020-06-30 01:03:05 -0400

    Fix.

--- test.py
+++ test.py
@@ -15,18 +15,18 @@ assert(lib is not None)
 import ctypes
 
 key = bytes(list(range(32)))
-data = bytes(list(range(32))*10)
+data = bytes(list(range(32))*100)
 
 ciphertext_ = ctypes.create_string_buffer(len(data)+16)
 key_ = ctypes.create_string_buffer(key)
 message_ = ctypes.create_string_buffer(bytes(data))
-length_ = ctypes.create_string_buffer(len(data).to_bytes(4, 'little'))
+length_ = ctypes.create_string_buffer(bytes([128, 12, 0, 0]))
 lib.server_encrypt_to_self(ciphertext_, key_, message_, length_)
 ciphertext = bytes(ciphertext_.raw)
 
 message_ = ctypes.create_string_buffer(len(ciphertext)-16)
 ciphertext_ = ctypes.create_string_buffer(bytes(ciphertext))
-length_ = ctypes.create_string_buffer(len(ciphertext).to_bytes(4, 'little'))
+length_ = ctypes.create_string_buffer([144, 12, 0, 0])
 lib.server_decrypt_from_self(message_, key_, ciphertext_, length_)
 data_ = bytes(message_.raw)
 
