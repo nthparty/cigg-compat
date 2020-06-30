9fdab67c52040863d09ea25eafe44c0db4d2c704
Andrei Lapets <a@lapets.io>
2020-06-30 01:12:25 -0400

    Test.

--- test.py
+++ test.py
@@ -15,25 +15,14 @@ assert(lib is not None)
 import ctypes
 
 key = bytes(list(range(32)))
-data = bytes(list(range(32))*100)
+data = bytes(list(range(32)))
 
-ciphertext_ = ctypes.create_string_buffer(len(data)+16)
+ciphertext_ = ctypes.create_string_buffer(len(data)+200)
 key_ = ctypes.create_string_buffer(key)
 message_ = ctypes.create_string_buffer(bytes(data))
 try:
     length_ = ctypes.create_string_buffer(len(data).to_bytes(4, 'little'))
 except:
-    length_ = ctypes.create_string_buffer(bytes([128, 12, 0, 0]))
+    length_ = ctypes.create_string_buffer(bytes([32, 0, 0, 0]))
 lib.server_encrypt_to_self(ciphertext_, key_, message_, length_)
-ciphertext = bytes(ciphertext_.raw)
-
-message_ = ctypes.create_string_buffer(len(ciphertext)-16)
-ciphertext_ = ctypes.create_string_buffer(bytes(ciphertext))
-try:
-    length_ = ctypes.create_string_buffer(len(ciphertext).to_bytes(4, 'little'))
-except:
-    length_ = ctypes.create_string_buffer(bytes([144, 12, 0, 0]))
-lib.server_decrypt_from_self(message_, key_, ciphertext_, length_)
-data_ = bytes(message_.raw)
-
-assert(data == data_)
+print(ciphertext_.raw)
