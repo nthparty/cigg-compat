d1c1387dda8c5fbf6b3bce33ef7734574f50d70b
Andrei Lapets <a@lapets.io>
2020-06-30 02:06:55 -0400

    Test.

--- test.py
+++ test.py
@@ -10,26 +10,3 @@ lib = canaries.load({
 })
 
 assert(lib is not None)
-
-# Check some of the cryptographic primitives it exports.
-import ctypes
-
-key = bytes(list(range(32)))
-data = bytes(list(range(32*10)))
-
-ciphertext_ = ctypes.create_string_buffer(len(data)+16)
-key_ = ctypes.create_string_buffer(key)
-message_ = ctypes.create_string_buffer(bytes(data))
-lib.server_encrypt_to_self(ciphertext_, key_, message_, len(data))
-ciphertext = bytes(ciphertext_.raw)
-
-message_ = ctypes.create_string_buffer(len(ciphertext)-16)
-ciphertext_ = ctypes.create_string_buffer(bytes(ciphertext))
-lib.server_decrypt_from_self(message_, key_, ciphertext_, len(ciphertext_))
-data_ = bytes(message_.raw)
-
-print([i for i in data])
-print()
-print([i for i in data_])
-
-assert(data == data_)
