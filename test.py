6e95628f845a3f5a723b2223f847d9145e2c0fe0
Andrei Lapets <a@lapets.io>
2020-06-30 01:21:13 -0400

    Test.

--- test.py
+++ test.py
@@ -17,12 +17,12 @@ import ctypes
 key = bytes(list(range(32)))
 data = bytes(list(range(32)))
 
-ciphertext_ = ctypes.create_string_buffer(len(data)+200)
+ciphertext_ = ctypes.create_string_buffer(len(data)+16)
 key_ = ctypes.create_string_buffer(key)
 message_ = ctypes.create_string_buffer(bytes(data))
 try:
     length_ = ctypes.create_string_buffer(len(data).to_bytes(4, 'little'))
 except:
-    length_ = ctypes.create_string_buffer(bytes([32, 0, 0, 0]))
+    length_ = ctypes.create_string_buffer(bytes([128, 12, 0, 0]))
 lib.server_encrypt_to_self(ciphertext_, key_, message_, length_)
 print(ciphertext_.raw)
