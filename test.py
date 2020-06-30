bdae0c95983c970bb23add45955e5518caaca06c
Andrei Lapets <a@lapets.io>
2020-06-30 00:46:46 -0400

    Add cryptographic primitives test.

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 """Load and confirm compatibility of CIGG for current environment."""
 
-import sys
-import platform
+# Check that the object file can be loaded and invoked.
 import canaries
-
-print("sys.maxsize: ", sys.maxsize)
-print("platform.system(): ", platform.system())
 
 lib = canaries.load({
     'Linux': ['./cigg.linux.so'],
@@ -14,3 +10,24 @@ lib = canaries.load({
 })
 
 assert(lib is not None)
+
+# Check some of the cryptographic primitives it exports.
+import secrets
+
+key = bytes(secrets.token_bytes(32))
+data = bytes(secrets.token_bytes(32*100))
+
+ciphertext_ = ctypes.create_string_buffer(len(data)+16)
+key_ = ctypes.create_string_buffer(key)
+message_ = ctypes.create_string_buffer(bytes(data))
+length_ = ctypes.create_string_buffer(len(data).to_bytes(4, 'little'))
+lib.server_encrypt_to_self(ciphertext_, key_, message_, length_)
+ciphertext = bytes(ciphertext_.raw)
+
+message_ = ctypes.create_string_buffer(len(ciphertext)-16)
+ciphertext_ = ctypes.create_string_buffer(bytes(ciphertext))
+length_ = ctypes.create_string_buffer(len(ciphertext).to_bytes(4, 'little'))
+lib.server_decrypt_from_self(message_, key_, ciphertext_, length_)
+data_ = bytes(message_.raw)
+
+assert(data == data_)
